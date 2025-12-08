4fde5f43d5eff7df392050cda1e0c8777375f565
Investigathon Equipo 4 <investigathonequipo4>
2025-12-08 14:39:59 +0000

    dejo script para grafico de latencia

---
+++ investigaton-NLP-main/src/analysis/Analysis3_latencia.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import numpy as np
+import pandas as pd
+import json
+import os
+
+class Analysis3: 
+    def __init__(self):
+        results_dir = (f"/home/ubuntu/investigaton-NLP/investigaton-NLP-main/data/results/500k10_judge_midiendoLatencia")
+        json_files = [f for f in os.listdir(results_dir) if f.endswith(".json")]
+
+        results = []
+        for filename in json_files:
+            filepath = os.path.join(results_dir, filename)
+            with open(filepath, "r") as f:
+                data = json.load(f)
+                results.append(data)
+
+        self.df = pd.DataFrame(results)
+
+            
+    def make_latency_chart(self):
+        metrics = self.df.groupby("question_type").agg(
+        tiempo_en_segs=("time taken to answer", "mean")  # Calcula el promedio (0.0 a 1.0)
+        ).sort_values(by="tiempo_en_segs", ascending=True).reset_index()
+
+        #momento grafico
+        plt.style.use('seaborn-v0_8-whitegrid') # O 'ggplot' si prefieres
+        fig, ax = plt.subplots(figsize=(10, 7))
+
+        # barras
+        barras = ax.barh(metrics['question_type'], metrics['tiempo_en_segs'], color='#4c72b0', zorder=3)
+        plt.subplots_adjust(left=0.2)  # esto porque me quedan cortadas
+        ax.set_xlim(0, 15) 
+
+
+        ax.set_title(' Tiempo promedio en segundos por cada tipo de Pregunta', fontsize=14, pad=15)
+
+        for barra in barras:
+            width = barra.get_width()
+            # El 3f para que agarre algunos decimales (se ve mas pro)
+            label_text = f'{width:.3f} segs ' 
+            
+            ax.text(width + 0.01,       
+                    barra.get_y() + barra.get_height()/2,
+                    label_text, 
+                    ha='left', va='center', fontsize=10, fontweight='bold', color='black')
+
+        plt.savefig("muestra500-latenciaPromedio.png")
+        plt.show()
+
+        return
--- investigaton-NLP-main/src/analysis/main.py
+++ investigaton-NLP-main/src/analysis/main.py
@@ -1,8 +1,12 @@
 from Analysis import Analysis
+from Analysis3_latencia import Analysis3
 
 def main():
     a = Analysis()
     a.make_bar_chart_wrong_answers_by_types()
+
+    b = Analysis3()
+    b.make_latency_chart()
 
 if __name__ == "__main__":
     main()
